a58e080568f6502d0384598746c2a0f61e985602
Oleksandr <lxbekas@gmail.com>
2024-10-25 21:09:14 +0300

    Add pages and images.

--- moon_web/src/App.js
+++ moon_web/src/App.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
        <h1>Moon</h1>
        <Footer/>
     </div>
