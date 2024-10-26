87712291f33a322a33e499c191165e537eb83af2
Nikita Pavlyk <nikita.pavlyk.official@gmail.com>
2024-10-26 09:20:23 +0200

    -added services db

--- back/src/main/java/com/development/moon/dev/service/RoleService.java
+++ back/src/main/java/com/development/moon/dev/service/RoleService.java
@@ -1,9 +1,11 @@
 package com.development.moon.dev.service;
 
 import com.development.moon.dev.model.Role;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface RoleService {
 
     Role save(Role role);
--- back/src/main/java/com/development/moon/dev/service/UserResponsesService.java
+++ back/src/main/java/com/development/moon/dev/service/UserResponsesService.java
@@ -2,10 +2,11 @@ package com.development.moon.dev.service;
 
 
 import com.development.moon.dev.model.UserResponses;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public interface UserResponsesService {
 
     UserResponses save(UserResponses ur);
