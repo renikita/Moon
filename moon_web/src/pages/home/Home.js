f552fb4462cd7f6661eb71c321e7cdde5f3df01f
Oleksandr <lxbekas@gmail.com>
2024-11-01 12:52:28 +0200

    Work with the main page, designed and styled part of the main page.

--- moon_web/src/pages/home/Home.js
+++ moon_web/src/pages/home/Home.js
@@ -1,13 +1,24 @@
- 
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './home.css'
+import "../../styles/fonts.css";
 
-import '../../styles/fonts.css'
 
 export default function Home() {
   return (
-   <div>
-    <h1>Home</h1>
-    
-   </div>
-  )
+    <div>
+      <div className="section-one">
+        <div className="content">
+          <h1>
+            Safe <br/>Software<br/> Development
+          </h1>
+          <div className="top-btn">
+            <Link to="/contacts" className="contact-btn">Contacts</Link>
+            <Link to="/services" className="services-btn">Services</Link>
+          </div>
+        </div>
+        <h2 className="about">About us</h2>
+      </div>
+    </div>
+  );
 }
