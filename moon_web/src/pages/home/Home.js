661d3bea3845cb678142ce37a45c1e536333248d
Oleksandr <lxbekas@gmail.com>
2024-11-01 22:39:06 +0200

    Work with the main page added a second section.

--- moon_web/src/pages/home/Home.js
+++ moon_web/src/pages/home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './home.css'
+import "./home.css";
+import "./home_media.css";
 import "../../styles/fonts.css";
-
+import teams from "./img/teams.png";
 
 export default function Home() {
   return (
@@ -10,14 +11,38 @@ export default function Home() {
       <div className="section-one">
         <div className="content">
           <h1>
-            Safe <br/>Software<br/> Development
+            Safe <br />
+            Software
+            <br /> Development
           </h1>
           <div className="top-btn">
-            <Link to="/contacts" className="contact-btn">Contacts</Link>
-            <Link to="/services" className="services-btn">Services</Link>
+            <Link to="/contacts" className="contact-btn">
+              Contacts
+            </Link>
+            <Link to="/services" className="services-btn">
+              Services
+            </Link>
           </div>
         </div>
         <h2 className="about">About us</h2>
+      </div>
+      <div className="section-two">
+        <img className="teams-img" src={teams} alt="teams" />
+        <h2>IT Outsourcing Services Company</h2>
+        <p>
+          Moon is a state-of-the-art app development studio specializing in
+          creating innovative and user-friendly mobile applications. With a
+          focus on sleek design and seamless functionality, Moon brings your
+          ideas to life with custom development solutions. Whether you need a
+          business app, a creative tool, or a mobile game, our team of
+          experienced developers ensures high performance and quality. At Moon,
+          we bring your vision to life, delivering apps that meet modern
+          technology demands and user expectations.
+        </p>
+        <Link to="/about" className="about-btn">
+          About us
+        </Link>
+        <h1>Partners</h1>
       </div>
     </div>
   );
