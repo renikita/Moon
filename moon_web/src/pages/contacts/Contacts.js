a8c10f882a1fb4dc9ecd09c71d2eb1f754150ad7
Oleksandr <lxbekas@gmail.com>
2024-11-21 13:52:54 +0200

    Add Contacts page with structured layout and ClientForm; enhance styling for improved user engagement.

--- moon_web/src/pages/contacts/Contacts.js
+++ moon_web/src/pages/contacts/Contacts.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./contacts.css";
+import ClientForm from "../../modules/clientForm/ClientForm";
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
-  )
+    <div className="cnt-container">
+      <div className="cnt-section-one">
+        <h2 className="cnt-one-h2">Contact us</h2>
+        <p className="cnt-one-p">
+          Got an amazing business idea? Let’s bring it to market together
+        </p>
+      </div>
+      <div className="cnt-section-two">
+        <ClientForm />
+      </div>
+    </div>
+  );
 }
