c2be7bdf8e0067af0ff6aee2476d6cf9b30fc794
Oleksandr <lxbekas@gmail.com>
2024-11-22 17:38:13 +0200

    Update Header and Footer components; replace logos and enhance Footer layout and styling for improved user experience.

--- moon_web/src/layouts/header/Header.js
+++ moon_web/src/layouts/header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo_black.png";
 import "./header.css";
 
 const Header = () => {
